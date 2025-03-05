d66b054737b6e7954dacbad2ab31a72a7b93f1a5
Lautaro Delgado <lautaromdelgado@gmail.com>
2025-03-05 08:07:42 -0300

    Configurar las rutas en el punto de entrada de la aplicación

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"barberia/internal/database/mysql"
-	"fmt"
+	"barberia/routes"
 	"log"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// Crear instancia de Echo
+	e := echo.New()
+
 	database := new(mysql.Config)
 	db, err := database.GetDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("DB: %v\n", db)
-
-	// Crear instancia de Echo
-	e := echo.New()
+	// Configurar todas las rutas
+	routes.SetupRoutes(e, db)
 
 	// Iniciar servidor
 	e.Logger.Fatal(e.Start(":8080"))
