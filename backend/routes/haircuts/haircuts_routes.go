2acc1c057be79635040a6fffad09154eb409e641
Lautaro Delgado <lautaromdelgado@gmail.com>
2025-03-11 15:27:26 -0300

    feat: Carga de la inicialización de las rutas para los cortes

---
+++ backend/routes/haircuts/haircuts_routes.go
@@ -0,0 +1,20 @@
+package haircuts_routes
+
+import (
+	"barberia/internal/handlers"
+	"barberia/internal/repository"
+	"barberia/internal/services"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+// SetUpRoutes configura las rutas de cortes
+func SetUpRoutes(e *echo.Echo, db *gorm.DB) {
+	haircutsRepo := repository.NewHaircutsRepository(db)               // Crear repositorio de cortes
+	haircutsServices := services.NewHaircutsServices(haircutsRepo)     // Crear servicio de cortes
+	haircutsHandlers := handlers.NewHaircutsHandlers(haircutsServices) // Crear manejador de cortes
+
+	// Obtener (GET)
+	e.GET("/haircuts", haircutsHandlers.GetAllHaircuts) // Obtener todos los cortes
+}
