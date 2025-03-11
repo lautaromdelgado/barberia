49b7c2b24564d891e9e846d08bb96ab615f5921b
lautaromdelgado <lautaromdelgado@gmail.com>
2025-03-11 08:24:34 -0300

    Implementar nueva ruta para el endpoint asociado a traer una barbería especifica por su id

--- backend/routes/barbershops/barbershops_routes.go
+++ backend/routes/barbershops/barbershops_routes.go
@@ -15,5 +15,6 @@ func SetUpRoutes(e *echo.Echo, db *gorm.DB) {
 	barbershopHandler := handlers.NewBarberShopsHandler(barbershopServices) // Crear un manejador de solicitud para las barberías
 
 	// Obtener (GET)
-	e.GET("/barbershops", barbershopHandler.GetAllBarberShops) // Obtener todas las barberías
+	e.GET("/barbershops", barbershopHandler.GetAllBarberShops)    // Obtener todas las barberías
+	e.GET("/barbershop/:id", barbershopHandler.GetByIDBarbershop) // Obtener una barbería por su ID
 }
