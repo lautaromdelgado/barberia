632cb61ff64ef56c1bf23cea9f865af9c4ec0082
lautaromdelgado <lautaromdelgado@gmail.com>
2025-03-11 02:56:31 -0300

    Configurar el manejador de las rutas para las barberías con sus endpoints correspondientes inicializando cada instancia desde el repositorio, servicios hasta los handlers

---
+++ backend/routes/barbershops/barbershops_routes.go
@@ -0,0 +1,19 @@
+package barbershops_routes
+
+import (
+	"barberia/internal/handlers"
+	"barberia/internal/repository"
+	"barberia/internal/services"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func SetUpRoutes(e *echo.Echo, db *gorm.DB) {
+	barbershopRepo := repository.NewBarbershopsRepository(db)               // Crear un nuevo repositorio para las barberías
+	barbershopServices := services.NewBarberShopsServices(barbershopRepo)   // Crear un nuevo servicio para las barberías
+	barbershopHandler := handlers.NewBarberShopsHandler(barbershopServices) // Crear un manejador de solicitud para las barberías
+
+	// Obtener (GET)
+	e.GET("/barbershops", barbershopHandler.GetAllBarberShops) // Obtener todas las barberías
+}
