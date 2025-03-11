c1e66675cb9c619b80c9d5665a3636e4c8fa42d0
lautaromdelgado <lautaromdelgado@gmail.com>
2025-03-11 02:55:36 -0300

    Pasar instacia de Echo y de la base de datos para poder darle acceso a todas las rutas que correspondan a endpoints asociados a las barberías

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,12 +1,15 @@
 package routes
 
 import (
+	barbershops_routes "barberia/routes/barbershops"
 	users_routes "barberia/routes/users"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
 
+// SetupRoutes inicializa todas las rutas de la aplicación
 func SetupRoutes(e *echo.Echo, db *gorm.DB) {
-	users_routes.SetupRoutes(e, db)
+	users_routes.SetupRoutes(e, db)       // Rutas de los usuarios
+	barbershops_routes.SetUpRoutes(e, db) // Rutas de las barberías
 }
