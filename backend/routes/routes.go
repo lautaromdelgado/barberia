27bbd75711137dce2121db30ac7b725335d2a854
Lautaro Delgado <lautaromdelgado@gmail.com>
2025-03-21 03:36:21 -0300

    feat: inyección de dependencias del contexto de Echo y puntero de base de datos, para las rutas del AUTH

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	auth_routes "barberia/routes/auth"
 	barbershops_routes "barberia/routes/barbershops"
 	barbershops_employee_routes "barberia/routes/barbershops_employee"
 	haircuts_routes "barberia/routes/haircuts"
@@ -16,4 +17,5 @@ func SetupRoutes(e *echo.Echo, db *gorm.DB) {
 	barbershops_routes.SetUpRoutes(e, db)         // Rutas de las barberías
 	haircuts_routes.SetUpRoutes(e, db)            // Rutas de los cortes
 	barbershops_employee_routes.SetUpRoute(e, db) // Rutas de los empleados de las barberías
+	auth_routes.SetUpRoutes(e, db)                // Rutas de autenticación
 }
