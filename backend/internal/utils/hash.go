cd759c0513fb02cd012e0d1b6061a5f3fbabaa95
Lautaro Delgado <lautaromdelgado@gmail.com>
2025-03-21 02:55:57 -0300

    feat: Implementar en el directorio utils, una función para hashear contraseñas (en este caso DNI de los usuarios)

---
+++ backend/internal/utils/hash.go
@@ -0,0 +1,12 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashedPassword hashea una contraseña
+func HashedPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
