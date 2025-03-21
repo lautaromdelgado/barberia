d0275ba55bb840c5c1c38c918cb49f34748e4242
Lautaro Delgado <lautaromdelgado@gmail.com>
2025-03-21 03:35:38 -0300

    feat: rutas e inyecciones de dependencias para el formateo de los endpoints

---
+++ backend/routes/auth/auth.go
@@ -0,0 +1,25 @@
+package auth_routes
+
+import (
+	handlers "barberia/internal/handlers/auth"
+	"barberia/internal/repository"
+	"barberia/internal/services"
+	auth_services "barberia/internal/services/auth"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func SetUpRoutes(e *echo.Echo, db *gorm.DB) {
+	// Uso de la clase UsersRepository (Para crear el usuario)
+	usersRepo := repository.NewUsersRepository(db)
+	// Uso de la clase UsersServices (Para la lógica de negocio en el registro de usuario)
+	usersService := services.NewUsersServices(usersRepo)
+	// Uso de la clase JWTService (Para la generación de tokens)
+	jwtServices := auth_services.NewJWTService()
+	// Uso de la clase AuthHandlers (Para el manejo de la autenticación)
+	authHandlers := handlers.NewAuthHandlers(jwtServices, usersService)
+
+	// Rutas de autenticación
+	e.POST("/register", authHandlers.Register) // Registrar un usuario
+}
